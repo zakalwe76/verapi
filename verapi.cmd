ecb8fb4231bf997f18a2334133ec519a337f166e
William Trotter <wtrotter@veracode.com>
2025-07-22 09:32:26 +0100

    Updated path to the credentials file to not be hardcoded

--- verapi.cmd
+++ verapi.cmd
@@ -1,1 +1,1 @@
-docker run --rm -it --mount "type=bind,source=C:\Users\WilliamTrotter\.veracode\,target=/root/.veracode/" -v "%cd%":/project verapi:latest %*
+docker run --rm -it --mount "type=bind,source=%USERPROFILE%\.veracode\,target=/root/.veracode/" -v "%cd%":/project verapi:latest %*
